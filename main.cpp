822fa516c8c949032cf6478aed41121f7ac21ab6
ramon <ramonmorcillocascales@gmail.com>
2019-02-18 06:37:19 -0800

    gamelloop, textures and sprites

--- main.cpp
+++ main.cpp
@@ -2,7 +2,16 @@
 // Author Ramon Morcillo @reymon359
 
 #include <SFML/Graphics.hpp>
+
+#include <game.h>
+
+using namespace sf;
+
 int main(int argc, char * args[])
 {
-   return 0;
+    game * match1;
+
+    match1 = new game(800, 600, "buck rogers");
+
+    return 0;
 }
