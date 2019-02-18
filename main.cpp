ee1c573bc670f2c8409e7bb19cf6350574d67e30
ramon <ramonmorcillocascales@gmail.com>
2019-02-18 04:34:00 -0800

    lets go

--- main.cpp
+++ main.cpp
@@ -1,37 +1,8 @@
+// Buck Rogers
+// Author Ramon Morcillo @reymon359
+
 #include <SFML/Graphics.hpp>
-
-int main()
+int main(int argc, char * args[])
 {
-    // Create the main window
-    sf::RenderWindow app(sf::VideoMode(800, 600), "SFML window");
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile("cb.bmp"))
-        return EXIT_FAILURE;
-    sf::Sprite sprite(texture);
-
-	// Start the game loop
-    while (app.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (app.pollEvent(event))
-        {
-            // Close window : exit
-            if (event.type == sf::Event::Closed)
-                app.close();
-        }
-
-        // Clear screen
-        app.clear();
-
-        // Draw the sprite
-        app.draw(sprite);
-
-        // Update the window
-        app.display();
-    }
-
-    return EXIT_SUCCESS;
+   return 0;
 }
